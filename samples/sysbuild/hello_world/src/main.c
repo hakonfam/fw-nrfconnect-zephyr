45c0e10be3be28155d339c532017acd87f251b96
Håkon Amundsen <haakon.amundsen@nordicsemi.no>
2025-08-11 07:46:12 +0200

    testing RTT on FLPR
    west build --pristine -d ~/ncs/build_app -b nrf54h20dk/nrf54h20/cpuapp zephyr/samples/sysbuild/hello_world --sysbuild --pristine -T sample.sysbuild.hello_world.nrf54h20dk_cpuapp_cpuflpr -- \
      -Dremote_CONFIG_USE_SEGGER_RTT=y \
      -Dremote_CONFIG_RTT_CONSOLE=y \
      -Dremote_CONFIG_LOG_BACKEND_RTT=y \
      -Dremote_CONFIG_LOG=y \
      -Dremote_CONFIG_LOG_PRINTK=y \
      -Dremote_CONFIG_UART_CONSOLE=n \
      -Dremote_CONFIG_CONSOLE=y \
      -Dremote_CONFIG_SEGGER_RTT_BUFFER_SIZE_UP=8192 \
      -Dremote_CONFIG_SEGGER_RTT_MODE_NO_BLOCK_SKIP=y
    
    Signed-off-by: Håkon Amundsen <haakon.amundsen@nordicsemi.no>

--- samples/sysbuild/hello_world/src/main.c
+++ samples/sysbuild/hello_world/src/main.c
@@ -3,11 +3,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/cache.h>
 
 int main(void)
 {
-	printk("Hello world from %s\n", CONFIG_BOARD_TARGET);
+	int counter = 0;
+
+	printk("Starting Hello World on %s\n", CONFIG_BOARD_TARGET);
+
+	while (true) {
+		printk("Hello world from %s - count: %d\n", CONFIG_BOARD_TARGET, counter++);
+		// Force cache flush after RTT write
+		sys_cache_data_flush_all();
+		k_sleep(K_SECONDS(5));
+	}
 
 	return 0;
 }
